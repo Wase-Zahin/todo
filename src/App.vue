73c3bdc42fbdd75205272c55857cbd5b09563ada
Wase-Zahin <w.zahin@yahoo.com>
2022-12-12 23:51:39 +0800

    app initialization

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ToDoItemVue label="My todo item"></ToDoItemVue>
+</template>
+
+<script>
+import ToDoItemVue from './components/ToDoItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ToDoItemVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>to do</h1>
+        <input type="checkbox" id="todo-item"/>
+        <label for="todo-item">{{label}}</label>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        label: { required: true, type: String },
+        done: { default: true, type: Boolean }
+    }
+}
+</script>
